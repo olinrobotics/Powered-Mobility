e81432ac44f2053e67e2f1bd979da511c1a8e4ff
yycho0108 <jchocholate@gmail.com>
2018-04-09 04:55:45 -0400

    added pulsed terrain mapping

---
+++ pomo_description/scripts/rotate_lidar.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float64
+import numpy as np
+
+def main():
+    rospy.init_node('rotate_lidar')
+    rate = rospy.get_param('~rate', default=50.0)
+    rate_rotate = rospy.get_param('~rate_rotate', default=5.0)
+
+    angle_min = rospy.get_param('~angle_min', default=30.0)
+    angle_max = rospy.get_param('~angle_max', default=50.0)
+
+    angle_min = np.deg2rad(angle_min)
+    angle_max = np.deg2rad(angle_max)
+
+    pub = rospy.Publisher('/pomo/lidar_controller/command', Float64, queue_size=10)
+
+    rate = rospy.Rate(rate)
+    msg = Float64()
+    while not rospy.is_shutdown():
+        t = rospy.Time.now().to_sec()
+        theta = (angle_max - angle_min) * np.sin(2*np.pi*rate_rotate*t) + (angle_min)
+        msg.data = theta
+        pub.publish(msg)
+        rate.sleep()
+
+if __name__ == "__main__":
+    main()
