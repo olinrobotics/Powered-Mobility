ad3c98d086ce16cb74b69752db6a2549ba7fa472
Jamie Cho <jchocholate@gmail.com>
2018-07-30 11:30:14 -0400

    stable rnet teleoperation

--- pwm_driver/src/rnet_node.cpp
+++ pwm_driver/src/rnet_node.cpp
@@ -1,4 +1,3 @@
-
 /* TEST :
 (PC) rosrun	rosserial_python serial_node.py _port:=/dev/ttyUSB0 _baud:=57600
 (Pi) rosrun pwm_driver rnet_node.py 
@@ -27,20 +26,34 @@ ros::Publisher bat_pub("battery", &bat_msg);
 
 geometry_msgs::Twist cmd_vel;
 ros::Subscriber<geometry_msgs::Twist> cmd_vel_sub("cmd_vel", cmd_vel_cb);
-std::chrono::high_resolution_clock::time_point last_cmd = sysnow();
-float cmd_vel_timeout = 0.5;
-char can_cmd_buf[64];
+std::chrono::high_resolution_clock::time_point last_rcv = sysnow(); // last cmd_vel receive ( from master )
+std::chrono::high_resolution_clock::time_point last_cmd = sysnow(); // last cmd_vel send ( to wheelchair )
 
-char* rosSrvrIp = "/dev/ttyS0";
+float rcv_timeout     = 0.5;
+float cmd_vel_rate    = 100; // 50 hz
+float cmd_vel_period  = (1.0 / cmd_vel_rate);
+
+char can_cmd_buf1[64];
+char can_cmd_buf2[64];
+
+//char* rosSrvrIp = "/dev/ttyS0";
 //char* rosSrvrIp = "192.168.16.64";
+char ros_port[] = "/dev/ttyS0";
+char can_port[] = "can0";
+
+float clip(float mn, float x, float mx){
+	if(x<mn) return mn;
+	if(mx<x) return mx;
+	return x;
+}
 
 int main(){
 	// ros first
-	nh.initNode(rosSrvrIp);//, "11311");//11311);
+	nh.initNode(ros_port);//, "11311");//11311);
 	nh.advertise(bat_pub);
 	nh.subscribe(cmd_vel_sub);
 
-	ROSCan roscan("can0");
+	ROSCan roscan(can_port);
 	roscan.init();
 	roscan.set_filter(0x1C0C0000, 0x1FFFF0FF); // battery
 	//roscan.set_timeout(0.01);
@@ -54,6 +67,12 @@ int main(){
 	tv.tv_usec = 10000;
 
 	std::chrono::high_resolution_clock::time_point last_send = sysnow();
+
+	//disable joy
+	for(int i=0; i<3; ++i){
+		roscan.send("0C000000#");
+	}
+
 	while(1){//ros::ok()){
 		std::chrono::high_resolution_clock::time_point now = sysnow();
 
@@ -63,19 +82,29 @@ int main(){
 		int rc = select(roscan._s_h+1, &rdfs, NULL, NULL, &tv);
 
 		if(!rc){
-			float dt = sysdt(last_send, now);
-			if (dt > 1.0){
-				// beep
-				//roscan.send("181C0100#0260000000000000");
-				last_send = now;
-			}else{
-				float cmd_dt = sysdt(last_cmd, now);
-				if(cmd_dt > cmd_vel_timeout ){cmd_vel.linear.x = cmd_vel.angular.z = 0.0;}
-				int cmd_x = 1.3 * + 100 * cmd_vel.angular.z;
-				int cmd_y = 1.4 * + 100 * cmd_vel.linear.x;
-				sprintf(can_cmd_buf, "02001100#%02X%02X", (uint8_t)cmd_x, (uint8_t)cmd_y);
-				roscan.send(can_cmd_buf);
+			//float dt = sysdt(last_send, now);
+			//if (dt > 1.0){
+			//	// beep
+			//	//roscan.send("181C0100#0260000000000000");
+			//	last_send = now;
+			//}else{
+			float rcv_dt = sysdt(last_rcv, now);
+			if(rcv_dt > rcv_timeout){cmd_vel.linear.x = cmd_vel.angular.z = 0.0;}
+
+			float cmd_dt = sysdt(last_cmd, now);
+			if(cmd_dt > cmd_vel_period){
+				int cmd_x1 = clip(-100, 1.3 * - 100 * cmd_vel.angular.z, 100);
+				int cmd_y1 = clip(-100, 1.4 * + 100 * cmd_vel.linear.x,  100);
+				int cmd_x2 = clip(-25, 1.3 * - 25 * cmd_vel.angular.z, 25);
+				int cmd_y2 = clip(-25, 1.4 * + 25 * cmd_vel.linear.x,  25);
+
+				sprintf(can_cmd_buf1, "02001100#%02X%02X", (uint8_t)cmd_x1, (uint8_t)cmd_y1);
+				sprintf(can_cmd_buf2, "02000200#%02X%02X", (uint8_t)cmd_x2, (uint8_t)cmd_y2);
+				roscan.send(can_cmd_buf1);
+				roscan.send(can_cmd_buf2);
+				last_cmd = now;
 			}
+			//}
 		}
 
 		if(FD_ISSET(roscan._s_h, &rdfs)){
@@ -98,5 +127,5 @@ float sysdt(std::chrono::high_resolution_clock::time_point& t0, std::chrono::hig
 }
 void cmd_vel_cb(const geometry_msgs::Twist& msg){
 	cmd_vel = msg;
-	last_cmd = sysnow();
+	last_rcv = sysnow();
 }
